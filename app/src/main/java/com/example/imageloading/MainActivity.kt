f92119df1090dbbe010628ad22f87a23aae7b69f
6310611030 <6310611030@student.tu.ac.th>
2023-03-31 11:55:10 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/imageloading/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.imageloading
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.example.imageloading.ui.theme.ImageLoadingTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ImageLoadingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Column() {
+                            DisplayImage("https://fastly.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0")
+                            DisplayImage("https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80")
+                        }
+                        Column() {
+                            DisplayImage("https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg")
+                            DisplayImage("https://media.istockphoto.com/id/1295274245/photo/random-multicolored-spheres-computer-generated-abstract-form-of-large-and-small-balls-3d.jpg?s=612x612&w=0&k=20&c=q7NOl28YxIIOqKu6em50VlKrg6ISFyVww_nLOCr5W_A=")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DisplayImage(src: String, modifier: Modifier = Modifier) {
+      AsyncImage(
+          model = ImageRequest.Builder(LocalContext.current)
+              .data(src)
+              .crossfade(true)
+              .build(),
+          contentDescription = "",
+          contentScale = ContentScale.Crop,
+          modifier = Modifier.size(225.dp)
+      )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ImageLoadingTheme {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Column() {
+                DisplayImage("https://fastly.picsum.photos/id/237/536/354.jpg?hmac=i0yVXW1ORpyCZpQ-CknuyV-jbtU7_x9EBQVhvT5aRr0")
+                DisplayImage("https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fA%3D%3D&w=1000&q=80")
+            }
+            Column() {
+                DisplayImage("https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg")
+                DisplayImage("https://media.istockphoto.com/id/1295274245/photo/random-multicolored-spheres-computer-generated-abstract-form-of-large-and-small-balls-3d.jpg?s=612x612&w=0&k=20&c=q7NOl28YxIIOqKu6em50VlKrg6ISFyVww_nLOCr5W_A=")
+            }
+        }
+    }
+}
+
+
